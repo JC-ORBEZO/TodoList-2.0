c9d07c43aea7897205763af3dffc367f80ae3a59
Jose <Jose.orbezo@alumnos.frgp.utn.edu.ar>
2021-07-30 10:15:43 -0300

    Commit de prueba, subida a remoto GitHub

---
+++ src/components/pages/taskList.js
@@ -0,0 +1,14 @@
+const TaskList = () => {
+    /*
+        https://todolistvrs2.netlify.app/
+        https://github.com/gustindvr/pending-tasks
+        https://whimsical.com/K6eKqyzyGErdCmqNCiKzxQ
+    */
+    return ( 
+        <>
+
+        </> 
+    );
+}
+ 
+export default TaskList;
